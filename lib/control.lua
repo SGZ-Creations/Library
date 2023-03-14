bb4f0d1a25ee43691afffacd586ea6b2a850d2ed
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2023-03-14 10:15:14 +0100

    message detail

--- lib/control.lua
+++ lib/control.lua
@@ -3,33 +3,33 @@ local function check_for_error(event)
 
   if script.active_mods["Insectitron"] then
       if settings.startup["Insectitron-issue"].value then
-          printer(
-            "The mod currenty has recipe that don't work due to items it uses.\nYou can dissable this message in startup settings if u don't want the warnings."
-          )
+      printer(
+        "\nInsectitron mod currenty has recipe that don't work due to items it uses.\nYou can dissable this message in startup settings if u don't want the warnings."
+      )
       end
   end
 
   if script.active_mods["SpaceMod"] then
     if settings.startup["SpaceMod-issue"].value then
-        game.print(
-          "The mod currenty has recipe that don't work due to items it uses Compatibilit is being worked on.\nYou can dissable this message in startup settings if u don't want the warnings."
-        )
+      game.print(
+        "\nSpaceMod currently has recipes that don't work due to items it uses Compatibilit is being worked on.\nYou can dissable this message in startup settings if u don't want the warnings."
+      )
     end
   end
 
   if script.active_mods["TeleportationEquipment"] then
     if settings.startup["Teleportation-issue"].value then
-        game.print(
-          "This mod is block by tech.\nYou can dissable this message in startup settings if u don't want the warnings."
-        )
+      game.print(
+        "\nThis mod has technolegy blockage.\nYou can dissable this message in startup settings if u don't want the warnings."
+      )
     end
   end
 
   if script.active_mods["osm-radioactivity"] then
     if settings.startup["radioactivity-issue"].value then
-        game.print(
-          "This mod is block by tech.\nYou can dissable this message in startup settings if u don't want the warnings."
-        )
+      game.print(
+        "\nThis mod has technolegy blockage.\nYou can dissable this message in startup settings if u don't want the warnings."
+      )
     end
   end
 end
